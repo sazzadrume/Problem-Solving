29f14eaed36b9602d4c82703eb233ef850f6398a
Sazzad Rume <rumesazzad@gmail.com>
2022-06-19 21:39:33 +0600

    Requires Observation

---
+++ lunchtime-109/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int v;
+		cin >> v;
+
+		string s = "";
+
+		printf("0");
+
+		for (int i = 0; i < v - 2; ++i)
+		{
+			printf("1");
+		}
+
+		printf("0\n");
+	}
+}
+
+//10101010101

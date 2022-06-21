89c0d803c64cbe23068f1ac3ac446fe266d3dd3f
Sazzad Rume <rumesazzad@gmail.com>
2022-06-21 09:39:05 +0600

    Intended Solution in O(log n)

--- leetcode/34.cpp
+++ leetcode/34.cpp
@@ -3,10 +3,27 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int start = nums.size(), end = -1;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                start = min(start, i);
-                end = max(end, i);
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] == target){
+        //         start = min(start, i);
+        //         end = max(end, i);
+        //     }
+        // }
+
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = (l + r) + 1 / 2;
+
+            if (nums[mid] == target) {
+                start = min(start, mid);
+                end = max(end, mid);
+            }
+            if (nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
             }
         }
 

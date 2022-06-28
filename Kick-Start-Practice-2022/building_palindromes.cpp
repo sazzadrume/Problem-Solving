5973fe50d6557318b3ccdd01b6c7e0a1da6f8001
Sazzad Rume <rumesazzad@gmail.com>
2022-06-28 11:08:06 +0600

    Modifying

--- Kick-Start-Practice-2022/building_palindromes.cpp
+++ Kick-Start-Practice-2022/building_palindromes.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 bool canFormPalindrome(string str)
 {
-  int count[256] = { 0 };
+  int count[128] = { 0 };
 
   for (int i = 0; str[i]; i++)
     count[str[i]]++;
 
   int odd = 0;
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < 128; i++) {
     if (count[i] & 1)
       odd++;
 

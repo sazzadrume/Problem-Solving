0d664b108ba6c10fa35864eb76279410a986b66c
Sazzad Rume <rumesazzad@gmail.com>
2022-08-06 13:56:13 +0600

    AC

---
+++ cf-652/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int a;
+        cin >> a;
+
+        if (a % 4 == 0) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+
+}

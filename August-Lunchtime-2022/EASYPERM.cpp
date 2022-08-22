a09c37305aa24922db2fe2663391f989fc38a954
Sazzad Rume <rumesazzad@gmail.com>
2022-08-22 23:42:48 +0600

    AC

---
+++ August-Lunchtime-2022/EASYPERM.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt;
+	cin >> tt;
+
+	while (tt--) {
+		int n;
+		cin >> n;
+
+		while (n > 0) {
+			cout << n << ' ';
+			n--;
+		}
+		cout << '\n';
+	}
+	return 0;
+}

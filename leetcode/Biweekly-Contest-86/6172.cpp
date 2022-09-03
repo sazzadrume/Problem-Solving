0cfce4edc021530f6b02cbad7b9c561a5c7a3406
Sazzad Rume <rumesazzad@gmail.com>
2022-09-03 20:53:18 +0600

    AC

---
+++ leetcode/Biweekly-Contest-86/6172.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        // for(int i = 2; i < n - 1; i++){
+        //     string result = (i, n);
+        //     if(!isPalindrome(result)){
+        //         return false;
+        //     }
+        // }
+        return false;
+    }
+    
+    
+};

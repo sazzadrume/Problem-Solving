aef7ce1e1a6b719648ffb2955fbb18eb9c0d8adc
Sazzad Rume <rumesazzad@gmail.com>
2022-09-22 21:36:42 +0600

    AC

---
+++ codechef-start-57/MARKSTW.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	cin >> a >> b;
+
+	if (a >= 2 * b) {
+		printf("YES");
+	}
+	else {
+		printf("NO");
+	}
+	return 0;
+}
